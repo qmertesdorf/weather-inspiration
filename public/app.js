d1dd6f5c7a546ddf0bc733f8d0df03ada363d3d3
Quinten Mertesdorf <quintenmertesdorf@gmail.com>
2018-07-20 19:42:32 +0000

    Add background image based on weather

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var x = document.getElementById("disp-info");
+    var disp = document.getElementById("disp-info");
     
     $("#get-location-btn").click(function(){
         getLocation();
@@ -8,7 +8,7 @@ $(document).ready(function(){
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition);
         } else {
-            x.innerHTML = "Geolocation is not supported by this browser.";
+            disp.innerHTML = "Geolocation is not supported by this browser.";
         }
     }
     function showPosition(position) {
@@ -16,13 +16,27 @@ $(document).ready(function(){
         let userLong = (position.coords.longitude);
         
         console.log(userLat + " " + userLong);
-        console.log(`https://api.openweathermap.org/data/2.5/weather?lat=${userLat}&lon=${userLong}&APPID=d9865046d7b35cf5a68299a454741a3a`)
         $.getJSON(`https://api.openweathermap.org/data/2.5/weather?lat=${userLat}&lon=${userLong}&APPID=d9865046d7b35cf5a68299a454741a3a`)
         .then(showWeather);    
     }
     function showWeather(data){
         console.log(data);
         console.log(`Weather for your location is: ${data.weather[0].description}`);
-        x.innerHTML = `Weather for your location is: ${data.weather[0].description}`;
+        // disp.innerHTML = `Weather for your location is: ${data.weather[0].description}`;
+        let img;
+        switch (data.weather[0].main) {
+            case "Rain":
+                console.log("Rain!");
+                img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT22R42juDi4Q2pZPAeb-1hSVYo27wn6HjquricAbnuOxhV5_cX"
+                break;
+            case "Clouds":
+                console.log("Clouds!");
+                img = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQASQIbASOtYSoFfIGhkXvGUu89J3qV87xuWHYOBZa7Cls1oNr9Ww"
+                break;
+            default:
+            console.log("yeeeah I screwed this up!");
+        }
+        console.log(img);
+        $("body").css("background-image", `url("${img}")`);
     }
 })
