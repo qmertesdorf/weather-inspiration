1eff74301a353d599d8cf288a1066409efb70d56
qmertesdorf <quintenmertesdorf@gmail.com>
2019-05-05 17:03:45 -0700

    add .gitignore, touch up code

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require("express"),
-    app = express(),
-    port = process.env.PORT,
-    bodyParser = require("body-parser");
+  app = express(),
+  port = process.env.PORT || 8000,
+  bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views"));
 
-app.get("/", function(req, res){
-    res.sendFile("index.html");
+app.get("/", function(req, res) {
+  res.sendFile("index.html");
 });
 
-
-app.listen(port, function(){
-    console.log("App is running on port!");
+app.listen(port, function() {
+  console.log("App is running on port: ", port);
 });
